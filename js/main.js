f61587304bcb9413aedee54adc757c49c3ae4419
maureenmbithe <112892541+maureenmbithe@users.noreply.github.com>
2022-10-07 13:12:23 +0300

    files added

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+const dayWeekNames = ["Sunday", "Monday","Tuesday","Wednesday","Thursday", "Friday","Saturday",]
+
+var century = 19
+var year =1989
+var month = 12
+var day = 7
+var gender = "m"
+var 
+
+function calculateNameAndDay (century,year,month,day)
+    var DayOfTheWeek = Math.round( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ), mod 7
+
+    var dayTheUserWasBorn = Math.round ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ), mod 7
+    var dayTheUserWasBorn = DAYS_OF_THE_WEEK[indexOfTheDay];
+    var name = null
+
+    if (gender ==='m'){
+        name =maleNames[indexOfTheDay]
+
+    } else if (gender ==='f')
+             name =femaleNames[indexOfTheDay]
+ alert
