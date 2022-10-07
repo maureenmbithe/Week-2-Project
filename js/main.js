6d05444f394b2a8db03ffa0ac46e0f02a2381ccd
maureenmbithe <112892541+maureenmbithe@users.noreply.github.com>
2022-10-07 21:09:25 +0300

    images deleted

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ var year =1989
 var month = 12
 var day = 7
 var gender = "m"
-var DayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ), % 7;
+var indexOfTheDay= ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ), % 7;
 
 
 
 function calculateNameAndDay (century,year,month,day)
-    var DayOfTheWeek = Math.round( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ), mod 7
+    var indexOfTheDay = Math.round( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ), mod 7
 
     var dayTheUserWasBorn = Math.round ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ), mod 7
     var dayTheUserWasBorn = DAYS_OF_THE_WEEK[indexOfTheDay];
